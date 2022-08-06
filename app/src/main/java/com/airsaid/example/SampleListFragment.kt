43e67a6796e9f4c1199f034f9d47715c5bdfd334
Airsaid <airsaid1024@gmail.com>
2022-08-06 22:11:12 +0800

    Update sample

--- app/src/main/java/com/airsaid/example/SampleListFragment.kt
+++ app/src/main/java/com/airsaid/example/SampleListFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.airsaid.sample.api.Register
@@ -27,7 +28,7 @@ import com.airsaid.sample.extension.permission.SamplePermission
 class SampleListFragment : Fragment() {
 
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-    return inflater.inflate(R.layout.fragment_sample_text, container, false)
+    return inflater.inflate(R.layout.fragment_sample_message, container, false)
   }
 
   @SuppressLint("SetTextI18n")
@@ -35,6 +36,9 @@ class SampleListFragment : Fragment() {
     super.onViewCreated(view, savedInstanceState)
     view.findViewById<TextView>(R.id.textView).apply {
       text = "Multiple sample annotations can be used together."
+    }
+    view.findViewById<Button>(R.id.print).setOnClickListener {
+      println("${System.currentTimeMillis()}: Hello World!")
     }
   }
 }
