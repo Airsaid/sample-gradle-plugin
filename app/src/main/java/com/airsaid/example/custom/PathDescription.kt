0e945581da9a9843be162c41662eacd975ad12ab
Airsaid <airsaid1024@gmail.com>
2022-08-12 18:02:28 +0800

    Add @PathDescription example

---
+++ app/src/main/java/com/airsaid/example/custom/PathDescription.kt
@@ -0,0 +1,9 @@
+package com.airsaid.example.custom
+
+import com.airsaid.sample.api.PathDescription
+
+/**
+ * @author airsaid
+ */
+@PathDescription(title = "Custom", desc = "Demonstrate how to customize extended support.")
+class PathDescription
